0d71403f3d3236b202deaee311dc4d8453d8aae3
achrefmechergui <achreffcb123achref@gamil.com>
2025-11-16 21:23:19 +0100

    done

---
+++ pizzamenu/src/Challange.js
@@ -0,0 +1,53 @@
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced", 
+    color: "#2662EA",
+  },
+  {
+    skill: "javaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and Github",
+    level: "intermediate",
+    color: "#E84F33",
+    
+  },
+
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Python",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
+
+// function Avatar (){
+
+// }
+
+// function Intro (){
+
+// }
+
+// function SkillList (){
+
+// }
+
+// function Skill (){
+
+// }
+
+// 💪 🤙 👶
--- pizzamenu/src/index.js
+++ pizzamenu/src/index.js
@@ -50,95 +50,97 @@ const pizzaData = [
 function App() {
   ///// parent component
   return (
-    <div>
+    <>
       <Header />
       <Menu />
       <Footer />
-    </div>
+    </>
   );
 }
 
 function Header() {
   return <h1 className="title">Fast React Pizza Co.</h1>;
 }
-/// 0 , false , "" , undefined , null
-function Menu(props) {
+
+//// wrapped the element (react fragment)
+
+function Menu() {
   const pizzas = pizzaData;
   // const pizzas = []
   const numPizzas = pizzas.length; //// 0
   return (
     <div>
       <h2 className="menu-title">Our Menu </h2>
-      {numPizzas && (
-        <div className="cards-container">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </div>
-      )}
+   
+      {numPizzas > 0 ?  ( 
+
+      <>
+             <p className="menu-introduction">
+          Authentic Italien cuisine , 6 creative dishes to choose from . All from
+          our stone oven  all organic , all delicious .
+        </p>
+          
+          <div className="cards-container">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </div>
+      </>
+      )    :  <p style={{textAlign:"center" , color:"red"}}>
+        We 're still working on our menu . Please come back later .. </p>}
     </div>
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
   /////// early return ....
-  if (props.pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
   return (
-    <section className="card">
-      <img
-        height={200}
-        src={props.pizzaObj.photoName}
-        alt={props.pizzaObj.name}
-      />
+    <section className={`card ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img height={200} src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div className="card-info">
-        <h2> {props.pizzaObj.name}</h2>
-        <p>{props.pizzaObj.ingredients}</p>
-        <p>{props.pizzaObj.price}</p>
+        <h2> {pizzaObj.name}</h2>
+        <p>{pizzaObj.ingredients}</p>
+
+
+        {/* {pizzaObj.soldOut ? 
+          <span>sold out</span> : <h1> {pizzaObj.price}</h1>
+        } */}
+        <p>{pizzaObj.soldOut ? "Sold out" : pizzaObj.price}</p>
       </div>
     </section>
   );
 }
 
 function Footer() {
-  const hour = new Date().getHours(); /// 21
+  const hour = new Date().getHours(); /// 20
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
-  // ///// conditionnal rendering with early return ...
-  // if (!isOpen){
-  //   return <p style={{textAlign:"center"}}>
-  //     We're happy to welcome you between {openHour}:00 and {closeHour}:00
-  //   </p>
-  // }
-
-  // return (
-  //  ////// conditional rendering with && operator ...
-  //   <footer>
-  //     {isOpen && (   //// true or false
-  //       <div>
-  //         <p>We're open until {closeHour}:00 , Come visit us or order online</p>
-  //         <button className="btn">Order</button>
-  //       </div>
-  //     )}
-  //   </footer>
-  // );
-
   return (
     <footer>
       {isOpen ? (
-        <div>
-          <p>We're open until {closeHour}:00 , Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
-          
           We're happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
         </p>
       )}
     </footer>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div>
+      <p>
+        We're open from {openHour}:00 | until {closeHour}:00 , Come visit us or
+        order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
